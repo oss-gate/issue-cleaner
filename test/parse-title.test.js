bd22e33289f9666a85ca9fe71ce50dc99541e1cf
Kimiaki Kuno <knokmki612@gmail.com>
2019-01-20 22:52:37 +0900

    Clean parse-title.test.js

--- test/parse-title.test.js
+++ test/parse-title.test.js
@@ -3,22 +3,16 @@ const format = require('date-fns/format')
 const isAfter = require('date-fns/is_after')
 const parse = require('date-fns/parse')
 
+const workshop = 'OSS Gate Workshop         :Tokyo : 2018-12-31: knokmki612 :OSS Gate Issue Cleaner: Work log'
+const meetup = 'OSS Gate Meetup :Tokyo : 2018-06-12'
+const other = 'aaaaaaaa'
+
+const parseWorkshopTitle = new ParseTitle(workshop)
+const parseMeetupTitle = new ParseTitle(meetup)
+const parseOtherTitle = new ParseTitle(other)
+
 describe('ParseTitle', () => {
-  let parseWorkshopTitle
-  let parseMeetupTitle
-  let parseOtherTitle
-
-  beforeEach(() => {
-    const workshop = 'OSS Gate Workshop         :Tokyo : 2018-12-31: knokmki612 :OSS Gate Issue Cleaner: Work log'
-    const meetup = 'OSS Gate Meetup :Tokyo : 2018-06-12'
-    const other = 'aaaaaaaa'
-
-    parseWorkshopTitle = new ParseTitle(workshop)
-    parseMeetupTitle = new ParseTitle(meetup)
-    parseOtherTitle = new ParseTitle(other)
-  })
-
-  test('Parsing an Issue Title', async () => {
+  test('Parsing an Issue Title', () => {
     const { title } = parseWorkshopTitle
 
     expect(title).toMatchObject([
@@ -31,49 +25,49 @@ describe('ParseTitle', () => {
     ])
   })
 
-  test('Parsing an Issue Date', async () => {
+  test('Parsing an Issue Date', () => {
     const { rawDate, date } = parseWorkshopTitle
 
     expect(rawDate === format(date, 'YYYY-MM-DD')).toBe(true)
   })
 
-  test('Parsing other title', async () => {
+  test('Parsing other title', () => {
     const { base } = parseOtherTitle
 
     expect(base).toBe('aaaaaaaa')
   })
 
-  test('Assures that the title follows a format of the workshop event', async () => {
+  test('Assures that the title follows a format of the workshop event', () => {
     const { isEventIssue } = parseWorkshopTitle
 
     expect(isEventIssue).toBe(true)
   })
 
-  test('Assures that the title follows at format of the meetup event', async () => {
+  test('Assures that the title follows at format of the meetup event', () => {
     const { isEventIssue } = parseMeetupTitle
 
     expect(isEventIssue).toBe(true)
   })
 
-  test('Assures that the title doesn\'t follow any format', async () => {
+  test('Assures that the title doesn\'t follow any format', () => {
     const { isEventIssue } = parseOtherTitle
 
     expect(isEventIssue).toBe(false)
   })
 
-  test('Assures that the title doesn\'t follow correct date string', async () => {
+  test('Assures that the title doesn\'t follow correct date string', () => {
     const { isEventIssue } = new ParseTitle('OSS Gate Meetup :Tokyo : 2018-06-12&13')
 
     expect(isEventIssue).toBe(false)
   })
 
-  test('Not process in a same day', async () => {
+  test('Not process in a same day', () => {
     const { date } = parseWorkshopTitle
 
     expect(isAfter(parse('2018-12-31'), date)).toBe(false)
   })
 
-  test('Process in after day', async () => {
+  test('Process in after day', () => {
     const { date } = parseWorkshopTitle
 
     expect(isAfter(parse('2019-01-01'), date)).toBe(true)
